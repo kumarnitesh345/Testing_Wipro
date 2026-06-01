6ab4ab71756e199f6440f3468aeaf0d0d3445904
kumarnitesh345 <nk1711336@gmail.com>
2026-06-01 16:47:07 +0530

    dependency remove commit

--- CucumberBDD2/src/test/java/StepDefinition/TestRunner.java
+++ CucumberBDD2/src/test/java/StepDefinition/TestRunner.java
@@ -1,22 +1,24 @@
 package StepDefinition;
 
-
 import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-features = "src/test/resources/features",
-glue = {"StepDefinition"},
-tags= "@Smoke",
-plugin = {
-"pretty",
-"html:target/htmlreports/report2.html",
-"json:target/jsonreports/report1.jason",
-"junit:target/junitreports/report1.junit"
-}
+        features = "src/test/resources/features",
+        glue = {"StepDefinition"},
+        tags = "@Smoke",
+        monochrome = true,
+
+        plugin = {
+                "pretty",
+                "html:target/htmlreports/report.html",
+                "json:target/jsonreports/report.json",
+                "junit:target/junitreports/report.xml"
+        }
 )
 
 public class TestRunner {
