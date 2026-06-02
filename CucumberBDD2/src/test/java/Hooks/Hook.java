c90a238cb50b2d9ee6482b980968ec7d280cf98c
kumarnitesh345 <nk1711336@gmail.com>
2026-06-02 12:52:06 +0530

    commit

--- CucumberBDD2/src/test/java/Hooks/Hook.java
+++ CucumberBDD2/src/test/java/Hooks/Hook.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hook {
 
@@ -12,14 +11,9 @@ public class Hook {
 
     public void setup() {
 
-        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver();
 
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-
-        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
 
         driver.manage().timeouts()
               .implicitlyWait(Duration.ofSeconds(10));
@@ -28,8 +22,12 @@ public class Hook {
     }
 
     public void closes() {
-        if(driver != null) {
+
+        if (driver != null) {
+
             driver.quit();
+
+            System.out.println("Browser session closed successfully.");
         }
     }
 }
